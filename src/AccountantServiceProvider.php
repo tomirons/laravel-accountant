466bfab1c3f2a3397e10f7b6583454dd1a142856
tomirons <tom.irons@hotmail.com>
2017-08-08 15:42:46 -0400

    register config, resources, and assets

--- src/AccountantServiceProvider.php
+++ src/AccountantServiceProvider.php
@@ -15,6 +15,8 @@ class AccountantServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerRoutes();
+        $this->registerResources();
+        $this->defineAssetPublishing();
     }
 
     /**
@@ -24,7 +26,9 @@ class AccountantServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/accountant.php', 'accountant'
+        );
     }
 
     /**
@@ -41,5 +45,35 @@ class AccountantServiceProvider extends ServiceProvider
         ], function () {
             $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         });
+    }
+
+    /**
+     * Register the Accountant resources.
+     *
+     * @return void
+     */
+    protected function registerResources()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'accountant');
+    }
+
+    /**
+     * Define the asset publishing configuration.
+     *
+     * @return void
+     */
+    public function defineAssetPublishing()
+    {
+        $this->publishes([
+            __DIR__ . '/../public/js' => public_path('vendor/accountant/js'),
+        ], 'accountant-assets');
+
+        $this->publishes([
+            __DIR__ . '/../public/css' => public_path('vendor/accountant/css'),
+        ], 'accountant-assets');
+
+        $this->publishes([
+            __DIR__ . '/../public/img' => public_path('vendor/accountant/img'),
+        ], 'accountant-assets');
     }
 }
