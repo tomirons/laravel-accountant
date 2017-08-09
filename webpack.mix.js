ea3f5c0849781d7513791ea72cb89e79971c927e
tomirons <tom.irons@hotmail.com>
2017-08-09 16:06:06 -0400

    configure webpack

---
+++ webpack.mix.js
@@ -0,0 +1,20 @@
+const mix = require('laravel-mix');
+const webpack = require('webpack');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix
+    .setPublicPath('public')
+    .js('resources/assets/js/app.js', 'public/js')
+    .less('resources/assets/less/app.less', 'public/css')
+    .copy('resources/assets/img', 'public/img')
+    .version();
