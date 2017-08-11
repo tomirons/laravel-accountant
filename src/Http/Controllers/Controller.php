da82b245e155aacfd40a38897469148dda733aa5
tomirons <tom.irons@hotmail.com>
2017-08-11 15:33:21 -0400

    initialize stripe client

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -3,7 +3,24 @@
 namespace TomIrons\Accountant\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
+use TomIrons\Accountant\Client;
 
 class Controller extends BaseController
 {
+    /**
+     * Instance of the Client.
+     *
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
 }
