7587d342a60e04f16c74d9f483db8479a484a379
tomirons <tom.irons@hotmail.com>
2017-08-15 16:28:46 -0400

    create interface for clients

---
+++ src/Contracts/Client.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace TomIrons\Accountant\Contracts;
+
+interface Client
+{
+    /**
+     * Get an object.
+     *
+     * @param $id
+     * @return $this
+     */
+    public function retrieve($id);
+
+    /**
+     * Create a new object.
+     * @param $data
+     * @return $this
+     */
+    public function create($data);
+
+    /**
+     * Update an object.
+     *
+     * @param $id
+     * @param $data
+     * @return $this
+     */
+    public function update($id, $data);
+
+    /**
+     * Delete an object.
+     *
+     * @param $id
+     * @return $this
+     */
+    public function delete($id);
+}
