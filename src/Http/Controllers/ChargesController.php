e05181487de44e2508577966970eb10526760566
tomirons <tom.irons@hotmail.com>
2017-08-17 09:16:03 -0400

    remove the constructor from the parent controller and inject in the separate controller

--- src/Http/Controllers/ChargesController.php
+++ src/Http/Controllers/ChargesController.php
@@ -8,14 +8,35 @@ use TomIrons\Accountant\Clients\Charge;
 class ChargesController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(Charge $charge)
+    {
+        $this->client = $charge;
+    }
+
+    /**
      * Return all charges.
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function index(Request $request, Charge $charge)
+    public function index(Request $request)
     {
-        return $charge->all()
+        return $this->client->all()
             ->currentPage($request->get('page', 1))
             ->paginate($request->url(), $request->query());
+    }
+
+    /**
+     * Show information about a charge.
+     *
+     * @param Request $request
+     * @return \Stripe\Charge
+     */
+    public function show($id)
+    {
+        return $this->client->retrieve($id);
     }
 }
--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -2,26 +2,14 @@
 
 namespace TomIrons\Accountant\Http\Controllers;
 
-use TomIrons\Accountant\Client;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     /**
-     * Instance of the Client.
+     * Instance of the client.
      *
-     * @var Client
+     * @var Object
      */
     protected $client;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @param Client $client
-     * @return void
-     */
-    public function __construct(Client $client)
-    {
-        $this->client = $client;
-    }
 }
