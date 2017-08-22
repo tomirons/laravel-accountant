8a24f7b5d23f16dc835a7bf7ee5079d11a79f31a
tomirons <tom.irons@hotmail.com>
2017-08-22 14:35:00 -0400

    switch to SASS

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ const mix = require('laravel-mix');
 mix
     .setPublicPath('public')
     .js('resources/assets/js/app.js', 'public/js')
-    .less('resources/assets/less/app.less', 'public/css')
+    .sass('resources/assets/sass/app.scss', 'public/css')
     .copy('resources/assets/img', 'public/img');
 
 if (mix.inProduction()) {
