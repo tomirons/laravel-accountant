1388a66fde1f9729dc8feea62a4d0a3081f5bf4b
tomirons <tom.irons@hotmail.com>
2017-08-23 16:06:56 -0400

    add doc blocks and fix formatting

--- src/ClientFactory.php
+++ src/ClientFactory.php
@@ -2,15 +2,20 @@
 
 namespace TomIrons\Accountant;
 
-
 use Illuminate\Contracts\Foundation\Application;
 
 class ClientFactory
 {
+    /**
+     * Instance of the application.
+     *
+     * @var Application
+     */
     protected $app;
 
     /**
-     * ClientFactory constructor.
+     * Create a new instance of the factory.
+     *
      * @param $app
      */
     public function __construct(Application $app)
@@ -18,10 +23,16 @@ class ClientFactory
         $this->app = $app;
     }
 
-
+    /**
+     * Magic method for retrieving a client.
+     *
+     * @param $method
+     * @return mixed
+     */
     public function __get($method)
     {
         $class = __NAMESPACE__ . '\\Clients\\' . ucfirst($method);
+
         return $this->app->make($class);
     }
 
