2544d308f7a6f6a6d7a47fed436fbc0bff94c50d
tomirons <tom.irons@hotmail.com>
2017-08-25 09:17:14 -0400

    add function for clickable rows

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,3 +5,9 @@ window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass');
 
 require('chartjs');
+
+$(document).ready(function($) {
+    $(".clickable").click(function() {
+        window.location = $(this).data("href");
+    });
+});
