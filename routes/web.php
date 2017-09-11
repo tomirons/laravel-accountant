b328120b77e3651668db3347e3ee8cb20607a20b
tomirons <tom.irons@hotmail.com>
2017-09-11 15:39:33 -0400

    add customer routes

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,9 @@ Route::get('/balance', 'BalanceController@index');
 Route::get('/charges', 'ChargeController@index');
 Route::get('/charges/{id}', 'ChargeController@show');
 
+// Customer routes...
+Route::get('/customers', 'CustomerController@index');
+Route::get('/customers/{id}', 'CustomerController@show');
+
 Route::get('/', 'HomeController@index');
 Route::get('{view}', 'HomeController@index')->where('view', '(.*)');
