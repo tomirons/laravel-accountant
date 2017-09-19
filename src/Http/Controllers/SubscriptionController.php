42c862af64474851f818b42e4f20e8223bb18a46
tomirons <tom.irons@hotmail.com>
2017-09-19 16:13:19 -0400

    create subscription controller

---
+++ src/Http/Controllers/SubscriptionController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace TomIrons\Accountant\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SubscriptionController extends Controller
+{
+    /**
+     * Return all subscriptions.
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $subscriptions = $this->factory->subscription->all()
+            ->currentPage($request->get('page', 1))
+            ->paginate($request->url(), $request->query());
+
+        return view('accountant::subscriptions.index', compact('subscriptions'));
+    }
+
+    /**
+     * Show information about a specific subscription.
+     *
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $subscription = $this->factory->subscription->retrieve($id);
+
+        return view('accountant::subscriptions.show', compact('subscription'));
+    }
+}
