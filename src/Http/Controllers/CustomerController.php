7de4614a420d2ccf7ba7c5cd56415b00e0cb2f56
tomirons <tom.irons@hotmail.com>
2017-09-19 09:07:14 -0400

    add show method to customer controller

--- src/Http/Controllers/CustomerController.php
+++ src/Http/Controllers/CustomerController.php
@@ -8,7 +8,7 @@ class CustomerController extends Controller
 {
     /**
      * Return all customers.
-     * 
+     *
      * @param Request $request
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
@@ -19,5 +19,18 @@ class CustomerController extends Controller
             ->paginate($request->url(), $request->query());
 
         return view('accountant::customers.index', compact('customers'));
+    }
+
+    /**
+     * Show information about a specific customer.
+     *
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $customer = $this->factory->customer->retrieve($id);
+
+        return view('accountant::customers.show', compact('customer'));
     }
 }
