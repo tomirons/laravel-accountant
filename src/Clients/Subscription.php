c15bb9cad0d9404c7bda58f3a8023b3d1d851d91
tomirons <tom.irons@hotmail.com>
2017-09-19 16:05:46 -0400

    create subscription client

---
+++ src/Clients/Subscription.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace TomIrons\Accountant\Clients;
+
+use TomIrons\Accountant\Client;
+
+class Subscription extends Client
+{
+    /**
+     * Available Stripe methods.
+     *
+     * @var array
+     */
+    protected $methods = [
+        'all', 'retrieve',
+    ];
+
+    /**
+     * Gets the name of the Stripe Client name.
+     * @return string
+     */
+    public function getClientName(): string
+    {
+        return 'Subscription';
+    }
+}
