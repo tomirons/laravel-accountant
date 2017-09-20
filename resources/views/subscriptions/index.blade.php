34990ffa3634e3fba00f0722a74a8443d376e48f
tomirons <tom.irons@hotmail.com>
2017-09-20 16:29:26 -0400

    create subscriptions listing view

---
+++ resources/views/subscriptions/index.blade.php
@@ -0,0 +1,31 @@
+@extends('accountant::app')
+
+@section('content')
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <table class="table table-striped table-hover">
+                <thead>
+                <tr>
+                    <th>Customer</th>
+                    <th>Billing</th>
+                    <th>Plan</th>
+                    <th>Created</th>
+                </tr>
+                </thead>
+                <tbody>
+                @Foreach($subscriptions as $subscription)
+                    <tr class="clickable" data-href="{{ url('accountant/subscriptions', $subscription->id) }}">
+                        <td><span class="text-primary">{{ $subscription->customer->email }}</span></td>
+                        <td class="interval"><span class="label label-primary">{{ $subscription->billing == 'charge_automatically' ? 'Auto' : 'Send' }}</span></td>
+                        <td>{{ $subscription->plan->name }}</td>
+                        <td>{{ Carbon\Carbon::createFromTimestamp($subscription->created)->format('Y/m/d h:i:s') }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+            <div class="text-center">
+                {{ $subscriptions->links() }}
+            </div>
+        </div>
+    </div>
+@endsection
