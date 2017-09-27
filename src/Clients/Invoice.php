30fb74724a58bbe57439f8bb6b3a1f7fa3f3d0e8
tomirons <tom.irons@hotmail.com>
2017-09-27 11:27:14 -0400

    create invoice client

---
+++ src/Clients/Invoice.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TomIrons\Accountant\Clients;
+
+use TomIrons\Accountant\Client;
+
+class Invoice extends Client
+{
+    /**
+     * Available Stripe methods.
+     *
+     * @var array
+     */
+    protected $methods = [
+        'all', 'retrieve',
+    ];
+
+    /**
+     * Gets the name of the Stripe Client name.
+     *
+     * @return string
+     */
+    public function getClientName(): string
+    {
+        return 'Invoice';
+    }
+}
