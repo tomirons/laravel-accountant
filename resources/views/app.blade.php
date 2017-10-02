c2e33641412bea5e167b1c7634ae74cc94373f27
tomirons <tom.irons@hotmail.com>
2017-10-02 16:29:57 -0400

    update navigation

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,20 +12,37 @@
 
     <body>
 
-        <nav class="navbar navbar-default">
+        <nav class="navbar navbar-default navbar-fixed-top">
             <div class="container">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="{{ url('accountant') }}">Accountant</a>
+                    <a href="{{ url('accountant') }}" class="navbar-brand visible-xs">Laravel Accountant</a>
                 </div>
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
+
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="{{ request()->is('accountant') ? 'active' : null }}">
+                            <a href="{{ url('accountant') }}">Dashboard</a>
+                        </li>
+                        <li class="{{ request()->is('accountant/charges', 'accountant/charges/*') ? 'active' : null }}">
+                            <a href="{{ url('accountant/charges') }}">Charges</a>
+                        </li>
+                        <li class="{{ request()->is('accountant/customers', 'accountant/customers/*') ? 'active' : null }}">
+                            <a href="{{ url('accountant/customers') }}">Customers</a></li>
+                        <li class="{{ request()->is('accountant/subscriptions', 'accountant/subscriptions/*') ? 'active' : null }}">
+                            <a href="{{ url('accountant/subscriptions') }}">Subscriptions</a>
+                        </li>
+                        <li class="{{ request()->is('accountant/invoices', 'accountant/invoices/*') ? 'active' : null }}">
+                            <a href="{{ url('accountant/invoices') }}">Invoices</a>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right hidden-xs">
+                        <li class="pull-right">
                             <a href="https://github.com/tomirons/laravel-accountant" target="_blank"><i class="fa fa-github"></i></a>
                         </li>
                     </ul>
@@ -35,25 +52,7 @@
 
         <div class="container">
             <div class="row">
-                <div class="col-md-3">
-                    <ul class="nav nav-pills nav-stacked">
-                        <li role="presentation" class="{{ request()->is('accountant') ? 'active' : null }}">
-                            <a href="{{ url('accountant') }}">Dashboard</a>
-                        </li>
-                        <li role="presentation" class="{{ request()->is('accountant/charges', 'accountant/charges/*') ? 'active' : null }}">
-                            <a href="{{ url('accountant/charges') }}">Charges</a>
-                        </li>
-                        <li role="presentation" class="{{ request()->is('accountant/customers', 'accountant/customers/*') ? 'active' : null }}">
-                            <a href="{{ url('accountant/customers') }}">Customers</a></li>
-                        <li role="presentation" class="{{ request()->is('accountant/subscriptions', 'accountant/subscriptions/*') ? 'active' : null }}">
-                            <a href="{{ url('accountant/subscriptions') }}">Subscriptions</a>
-                        </li>
-                        <li role="presentation" class="{{ request()->is('accountant/invoices', 'accountant/invoices/*') ? 'active' : null }}">
-                            <a href="{{ url('accountant/invoices') }}">Invoices</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-9">
+                <div class="col-md-12">
                     @yield('content')
                 </div>
             </div>
