6a1109c82a5a0f4c3a444be8a5fd93aa2d6dc670
tomirons <tom.irons@hotmail.com>
2017-10-03 12:01:39 -0400

    wrap the routes into a group

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Balance routes...
-Route::get('/balance', 'BalanceController@index');
+Route::group(['middleware' => config('accountant.middleware')], function () {
+    // Balance routes...
+    Route::get('/balance', 'BalanceController@index');
 
-// Charge routes...
-Route::get('/charges', 'ChargeController@index');
-Route::get('/charges/{id}', 'ChargeController@show');
-Route::get('/charges/{type?}/{id?}', 'ChargeController@index');
+    // Charge routes...
+    Route::get('/charges', 'ChargeController@index');
+    Route::get('/charges/{id}', 'ChargeController@show');
+    Route::get('/charges/{type?}/{id?}', 'ChargeController@index');
 
-// Customer routes...
-Route::get('/customers', 'CustomerController@index');
-Route::get('/customers/{id}', 'CustomerController@show');
+    // Customer routes...
+    Route::get('/customers', 'CustomerController@index');
+    Route::get('/customers/{id}', 'CustomerController@show');
 
-// Subscription routes...
-Route::get('/subscriptions', 'SubscriptionController@index');
-Route::get('/subscriptions/{id}', 'SubscriptionController@show');
+    // Subscription routes...
+    Route::get('/subscriptions', 'SubscriptionController@index');
+    Route::get('/subscriptions/{id}', 'SubscriptionController@show');
 
-// Invoice routes...
-Route::get('/invoices', 'InvoiceController@index');
-Route::get('/invoices/{id}', 'InvoiceController@show');
-Route::get('/invoices/{type?}/{id?}', 'InvoiceController@index');
+    // Invoice routes...
+    Route::get('/invoices', 'InvoiceController@index');
+    Route::get('/invoices/{id}', 'InvoiceController@show');
+    Route::get('/invoices/{type?}/{id?}', 'InvoiceController@index');
 
-Route::get('/', 'HomeController@index');
-Route::get('{view}', 'HomeController@index')->where('view', '(.*)');
+    Route::get('/charts', 'HomeController@charts');
+    Route::get('/', 'HomeController@index');
+    Route::get('{view}', 'HomeController@index')->where('view', '(.*)');
+});
