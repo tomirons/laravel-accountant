76335eda39ab47b8ed04c8ea8d5eff6fb89bc74d
tomirons <tom.irons@hotmail.com>
2017-10-04 12:37:12 -0400

    add cache configuration and update comment blocks

--- config/accountant.php
+++ config/accountant.php
@@ -8,6 +8,7 @@ return [
     |
     | This value is the the amount of results to display on each page.
     */
+
     'pagination' => [
         'limit' => 15,
     ],
@@ -20,6 +21,23 @@ return [
     | This is the middleware used for all routes of the package. By default
     | it uses the 'auth' middleware, but it can be configured how ever
     | you'd like. You can use a string of middleware(s) or an array.
+    |
+    | Supported types: string, array
     */
-    'middleware' => 'auth'
+
+    'middleware' => 'auth',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Settings
+    |--------------------------------------------------------------------------
+    |
+    | Here you can change the settings that are used when utilizing the cache
+    | system.
+    */
+
+    'cache' => [
+        'driver' => 'file',
+        'minutes' => 60
+    ]
 ];
