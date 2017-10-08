f3f0f1efb777fd79073e083a5e659c1cf866d8e0
tomirons <tom.irons@hotmail.com>
2017-10-08 15:09:12 -0400

    update routes

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::group(['middleware' => config('accountant.middleware', 'auth')], function
     Route::get('/invoices/{type?}/{id?}', 'InvoiceController@index');
 
     Route::get('/', 'HomeController@index');
-    Route::get('/charts', 'HomeController@charts');
+    Route::get('/data', 'HomeController@data');
+    Route::get('/data/refresh', 'HomeController@refresh');
     Route::get('{view}', 'HomeController@index')->where('view', '(.*)');
 });
