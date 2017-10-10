63c570fd911b6bb5b829d0566e3ba59e54169f42
tomirons <tom.irons@hotmail.com>
2017-10-10 09:25:50 -0400

    add more route methods

--- src/Http/Controllers/HomeController.php
+++ src/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace TomIrons\Accountant\Http\Controllers;
 
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     /**
@@ -11,6 +13,38 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('accountant::dashboard');
+        return view('accountant::dashboard')->with([
+            'stats' => $this->data(new Request)
+        ]);
+    }
+
+    /**
+     * Refresh all data.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function refresh()
+    {
+        $this->cabinet->refresh();
+
+        return redirect()->back();
+    }
+
+    /**
+     * Generate array based on the start and end date.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function data(Request $request)
+    {
+        if ($request->has(['start', 'end'])) {
+            session()->put([
+                'accountant.start' => Carbon::createFromTimestamp($request->get('start')),
+                'accountant.end' => Carbon::createFromTimestamp($request->get('end'))
+            ]);
+        }
+
+        return $this->cabinet->generate();
     }
 }
