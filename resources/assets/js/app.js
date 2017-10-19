c00422712e5accff72c42684158c9e2aabece4ac
tomirons <tom.irons@hotmail.com>
2017-10-19 11:59:45 -0400

    finish dashboard vue instance

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,13 +1,76 @@
-window._ = require('lodash');
+require('./bootstrap')
 
-window.$ = window.jQuery = require('jquery');
+import datePicker from 'vue-bootstrap-datetimepicker';
+import { Line } from 'vue-chartjs';
 
-require('bootstrap-sass');
+var dashboard = new Vue({
+    el: '#dashboard',
+    data: {
+        data: null,
+        filter: {
+            start: null,
+            end: null,
+            show: false,
+            max: moment().format('MM/DD/YYYY')
+        }
+    },
+    components: {
+        datePicker,
+        'line-chart': {
+            extends: Line,
+            props: ['data', 'options'],
+            watch: {
+                data: function () {
+                    this.renderChart(this.data, {
+                        legend: {
+                            display: false
+                        }
+                    })
+                }
+            }
+        }
+    },
+    methods: {
+        applyFilter: function () {
+            axios.post('/accountant/data', {
+                start: this.filter.start.unix(),
+                end: this.filter.end.unix()
+            })
+                .then(function (response) {
+                    dashboard.setData(response.data);
+                    dashboard.toggleFilter();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        setData: function (data) {
+            this.data = data;
+        },
+        setFilters: function (filters) {
+            this.filter.start = moment(filters.start);
+            this.filter.end = moment(filters.end);
+        },
+        toggleFilter: function () {
+            this.filter.show = !this.filter.show;
+        }
+    },
+    created: function () {
+        this.setFilters(filterData);
+    },
+    watch: {
+        'filter.end': function () {
+            this.applyFilter();
+        },
+        'filter.start': function () {
+            this.applyFilter();
+        }
+    }
+});
 
-require('chartjs');
-
-$(document).ready(function($) {
+$(function() {
     $(".clickable").click(function() {
         window.location = $(this).data("href");
     });
+    $('[data-toggle="tooltip"]').tooltip();
 });
